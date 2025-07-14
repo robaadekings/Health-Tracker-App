1bb0313478435cc39e9d3f6f1fc3324adad9a802
robaadekings01@gmail.com <robaadekings01@gmail.com>
2025-07-14 20:34:58 +0300

    "routes done and tested with authentication to prove if they are fully working"

---
+++ healthtrack-backend/controllers/authController.js
@@ -0,0 +1,46 @@
+const User = require('../models/User');
+const generateToken = require('../utils/generateToken');
+
+// @desc    Register a new user
+// @route   POST /api/auth/register
+const registerUser = async (req, res) => {
+    const { name, email, password, age, height, weight, goals } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const user = await User.create({ name, email, password, age, height, weight, goals });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400).json({ message: 'Invalid user data' });
+    }
+};
+
+// @desc    Login user
+// @route   POST /api/auth/login
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+    });
+};
+
+module.exports = { registerUser, loginUser };
